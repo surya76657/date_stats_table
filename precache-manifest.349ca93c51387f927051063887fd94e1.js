04ef72ce3a27fae94ebe1c0b33dc7fcb607d91d8
surya76657 <surya76657@gmail.com>
2019-11-03 20:02:26 +0530

    Updates

---
+++ precache-manifest.349ca93c51387f927051063887fd94e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2239583dc754ce3da00e8ea91061af4",
+    "url": "/date_stats_table/index.html"
+  },
+  {
+    "revision": "640de266add1f0ba5a52",
+    "url": "/date_stats_table/static/css/2.a3455d1a.chunk.css"
+  },
+  {
+    "revision": "e9d9c1c59e5533ede02d",
+    "url": "/date_stats_table/static/css/main.ff2fb4bd.chunk.css"
+  },
+  {
+    "revision": "640de266add1f0ba5a52",
+    "url": "/date_stats_table/static/js/2.06fa9868.chunk.js"
+  },
+  {
+    "revision": "e9d9c1c59e5533ede02d",
+    "url": "/date_stats_table/static/js/main.75da91d2.chunk.js"
+  },
+  {
+    "revision": "4791e350525bc02a6822",
+    "url": "/date_stats_table/static/js/runtime~main.6f92dab2.js"
+  }
+]);
